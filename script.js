834a58cf11eba44589afd8f7eccbef9a55564ec5
agilang komporgass <agilangsakti09@gmail.com>
2025-10-07 12:30:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click'.function() {
-  alert('Welcome To My Website');
+tombolSapa.addEventListener('click', function() {
+  alert('Welcome To My GILWebsite');
 });
