8c5dd9d0d4f8acb93e1d41f02c523d9207913249
agilang komporgass <agilangsakti09@gmail.com>
2025-10-07 12:05:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ cont tombolSapa = document.getElementById('sapaButton'
 //2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click'.function(){
   //3. jalankan kode ini ketika tombol di-klik
-  alert('Welcome To My Website :)');
+  alert('Welcome To My Website');
 });
